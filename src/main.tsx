3b259c4e4191461750633b1dd85bafb1f67793cc
Gusarov Anatoly <gusarov.tolia@mail.ru>
2026-01-15 00:48:50 +0500

    chore: установить зависимости tailwindcss, mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { StyledEngineProvider } from "@mui/material/styles";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <StyledEngineProvider enableCssLayer>
+            <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+            <App />
+        </StyledEngineProvider>
+    </StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [['babel-plugin-react-compiler']],
-			},
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+    plugins: [
+        tailwindcss(),
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
